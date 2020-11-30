060c15f5d409165b687c63d7834aa404a22f858f
Jack Nichols <jacknichols@jacks-mbp.home>
2020-11-30 23:16:37 +0000

    created theme  set up, added sass, tailwind, material for project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import "./App.scss";
+import { Button, Typography } from '@material-ui/core';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return <div className="App">
+    <Button>Buy now</Button>
+    <Typography variant="h1" color="primary">This is my typography</Typography>
+    <Typography variant="h2" color="primary">This is my typography</Typography>
+    <Typography variant="h3" color="primary">This is my typography</Typography>
+  </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0D1D2B',
+      contrastText: 'white',
+    },
+    secondary: {
+      main: '#28AEF2',
+      contrastText: 'white',
+    }
+  },
+  typography: {
+    body2: "Bebas Neue",
+    fontFamily: "poppins"
+  },
+  shape: {
+    borderRadius: 30
+  },
+  spacing: 100,
+  overrides: {
+    MuiButton: {
+      root: { textTransform: 'none', padding: '.8rem 4rem', fontSize: '16px' }
+    }
+  },
+  props: {
+    MuiButton: {
+      disableRipple: true,
+      variant: 'contained',
+      color: 'secondary',
+    }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
